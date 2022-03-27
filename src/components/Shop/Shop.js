e43ed135c9ced04486aef7e254cd4a34e376a17c
Animesh Mondol <51692385+AnimeshMondol@users.noreply.github.com>
2022-03-27 19:59:37 +0600

    shop structure added

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Shop.css'
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className='product-container'>
+                <h1>product list</h1>
+            </div>
+            <div className='cart-container'>
+                <h1>Ordered Item</h1>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Shop;
